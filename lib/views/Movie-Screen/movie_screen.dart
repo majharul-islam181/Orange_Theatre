91321149dfcb7afb1d3ed860ce1f018a3a1553cd
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2024-08-30 17:00:52 +0600

    initial movie page done

--- lib/views/Movie-Screen/movie_screen.dart
+++ lib/views/Movie-Screen/movie_screen.dart
@@ -10,6 +10,47 @@ class MovieScreen extends StatefulWidget {
 }
 
 class _MovieScreenState extends State<MovieScreen> {
+  List<String> imageUrls = [
+    'url1',
+    'url2',
+    'url3',
+    'url1',
+    'url2',
+    'url3',
+    'url1',
+    'url2',
+    'url3',
+    'url1',
+    'url2',
+    'url3',
+    'url1',
+    'url2',
+    'url3',
+  ]; // Replace with your image URLs
+  List<String> names = [
+    'Name1',
+    'Name2',
+    'Name3',
+    'Name1',
+    'Name2',
+    'Name3',
+    'Name1',
+    'Name2',
+    'Name3',
+    'Name1',
+    'Name2',
+    'Name3',
+    'Name1',
+    'Name2',
+    'Name3',
+  ];
+
+  List<Map<String, dynamic>> genre = [
+    {"id": 28, "name": "Action"},
+    {"id": 35, "name": "Comedy"},
+    {"id": 878, "name": "Science Fiction"}
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,25 +72,37 @@ class _MovieScreenState extends State<MovieScreen> {
               ),
             ),
             Container(
-              height: MediaQuery.of(context).size.height * .35,
-              decoration: BoxDecoration(
-                  color: Colors.blue[900],
-                  borderRadius: const BorderRadius.only(
-                      bottomLeft: Radius.circular(20),
-                      bottomRight: Radius.circular(20))),
+              height: MediaQuery.of(context).size.height * 0.35,
+              decoration: const BoxDecoration(
+                // color: Colors.blue[900],
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(770),
+                  bottomRight: Radius.circular(40),
+                ),
+              ),
+              child: ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(400),
+                  bottomRight: Radius.circular(5),
+                ),
+                child: Image.network(
+                  'https://th.bing.com/th/id/R.78ab058529b639453db95b7ed2a5011f?rik=q%2fJtTs0jpSGRpg&pid=ImgRaw&r=0',
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
-            // Positioned(
-            //     top: MediaQuery.of(context).size.height * .3,
-            //     left: 16,
-            //     child: ClipRRect(
-            //       borderRadius: BorderRadius.circular(40),
-            //       child: Image.network(
-            //         'https://th.bing.com/th/id/R.78ab058529b639453db95b7ed2a5011f?rik=q%2fJtTs0jpSGRpg&pid=ImgRaw&r=0',
-            //         height: 140,
-            //         width: 100,
-            //       ),
-            //     )),
-
+            Positioned(
+                top: MediaQuery.of(context).size.height * .22,
+                left: 30,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(14),
+                  child: Image.network(
+                    'https://th.bing.com/th/id/R.b47abe620899296505b369ca1aea7539?rik=Vp2ncEwuYPT5%2fQ&riu=http%3a%2f%2fimages2.fanpop.com%2fimage%2fphotos%2f13000000%2fSpider-Man-spider-man-13062150-1280-1572.jpg&ehk=193u8f4xtMXTNRqmwccMThPn1irEykUWdkBqLse9oJU%3d&risl=&pid=ImgRaw&r=0',
+                    height: 100,
+                    width: 80,
+                    fit: BoxFit.cover,
+                  ),
+                )),
             Positioned(
                 top: MediaQuery.of(context).size.height * .3,
                 right: 16,
@@ -76,6 +129,10 @@ class _MovieScreenState extends State<MovieScreen> {
                 )),
           ],
         ),
+
+        const SizedBox(
+          height: 15,
+        ),
         Row(
           children: [
             Text(
@@ -86,9 +143,10 @@ class _MovieScreenState extends State<MovieScreen> {
           ],
         ),
         const SizedBox(
-          height: 5,
+          height: 10,
         ),
         Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Expanded(
               child: Row(
@@ -109,37 +167,84 @@ class _MovieScreenState extends State<MovieScreen> {
               ),
             ),
             Expanded(
-              child: Text(
-                'Minutes',
-                style: GoogleFonts.poppins(
-                    fontSize: 13, fontWeight: FontWeight.w400),
+              child: Row(
+                children: [
+                  SvgPicture.asset(
+                    'assets/icons/ecommerce-money-svgrepo-com.svg',
+                    height: 20,
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    'Revenue',
+                    style: GoogleFonts.poppins(
+                        fontSize: 13, fontWeight: FontWeight.w400),
+                  ),
+                ],
               ),
             ),
             Expanded(
-              child: Text(
-                'Action',
-                style: GoogleFonts.poppins(
-                    fontSize: 13, fontWeight: FontWeight.w400),
-              ),
-            ),
+                child: Row(
+              children: [
+                Image.asset(
+                  'assets/icons/production_house.png',
+                  height: 30,
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  'US',
+                  style: GoogleFonts.poppins(
+                      fontSize: 13, fontWeight: FontWeight.w400),
+                )
+              ],
+            )),
           ],
         ),
 
-        const SizedBox(height: 10,),
+        const SizedBox(
+          height: 10,
+        ),
 
-        //product companies
-        Row(
+        //production companies
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            CircleAvatar(
-              backgroundImage: Image.asset('name'),
-              child: const Text('data'),
-            )
+            Text(
+              "Production Companies",
+              style: GoogleFonts.poppins(
+                  fontSize: 17, fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+                height: 100,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: imageUrls.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Column(
+                          children: [
+                            // CircleAvatar(
+                            //   backgroundImage: NetworkImage(imageUrls[index]),
+                            //   radius: 30,
+                            // ),
+                            const SizedBox(height: 8),
+                            Text(
+                              names[index],
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                      );
+                    })),
           ],
         ),
-
-        
-     
-             
       ],
     ));
   }
