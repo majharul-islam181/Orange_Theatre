52f1e3b1021b1fbc364e941f24ed8808d708e5fe
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2024-08-31 18:42:32 +0600

    cashing api data trending movies and upcoming movies

--- lib/bloc/trending_movies_bloc/trending_movies_bloc.dart
+++ lib/bloc/trending_movies_bloc/trending_movies_bloc.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
 import 'package:orange_theatre/data/response/api_response.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../../models/models.dart';
 import '../../repository/repository.dart';
 part 'trending_movies_event.dart';
@@ -19,14 +23,30 @@ class TrendingMoviesBloc
       FetchTrendingMoviesEvent event, Emitter<TrendingMoviesState> emit) async {
     emit(state.copyWith(trendingMoviesList: ApiResponse.loading()));
 
+    // caching data
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    String? cashedData = sharedPreferences.getString('trendingMoviesData');
+
+    if (cashedData != null) {
+      TrendingMoviesModel cashedMovies =
+          TrendingMoviesModel.fromJson(json.decode(cashedData));
+      emit(state.copyWith(
+          trendingMoviesList: ApiResponse.completed(cashedMovies)));
+    }
 
     try {
+      // Fetch data from the API
       final trendingList =
           await trendingMoviesRepository.fetchTrendingMoviesList();
       emit(state.copyWith(
           trendingMoviesList: ApiResponse.completed(trendingList)));
+      // Cache the fetched data for future use
+      sharedPreferences.setString(
+          'trendingMoviesData', json.encode(trendingList.toJson()));
     } catch (error) {
-      print("error in trending api bloc");
+      if (kDebugMode) {
+        print("error in trending api bloc");
+      }
       emit(state.copyWith(
           trendingMoviesList: ApiResponse.error(error.toString())));
     }
--- lib/bloc/upcoming_movies_bloc/upcoming_movies_bloc.dart
+++ lib/bloc/upcoming_movies_bloc/upcoming_movies_bloc.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:orange_theatre/data/response/api_response.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../../models/models.dart';
 import '../../repository/repository.dart';
 part 'upcoming_movies_event.dart';
@@ -18,12 +21,27 @@ class UpcomingMoviesBloc
   Future<void> _fetchTrendingMovies(
       FetchUpcomingMoviesEvent event, Emitter<UpcomingMoviesState> emit) async {
     emit(state.copyWith(upcomingMoviesList: ApiResponse.loading()));
+    
+    // caching data
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    String? cashedData = sharedPreferences.getString('upcomingMoviesData');
+
+    if (cashedData != null) {
+      UpcomingMoviesModel cashedMovies =
+          UpcomingMoviesModel.fromJson(json.decode(cashedData));
+      emit(state.copyWith(
+          upcomingMoviesList: ApiResponse.completed(cashedMovies)));
+    }
 
     try {
+      // Fetch data from the API
       final trendingList =
           await upcomingMoviesRepository.fetchUpcomingMoviesList();
       emit(state.copyWith(
           upcomingMoviesList: ApiResponse.completed(trendingList)));
+           // Cache the fetched data for future use
+            sharedPreferences.setString(
+          'upcomingMoviesData', json.encode(trendingList.toJson()));
     } catch (error) {
       emit(state.copyWith(
           upcomingMoviesList: ApiResponse.error(error.toString())));
